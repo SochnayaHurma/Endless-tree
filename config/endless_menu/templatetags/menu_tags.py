d10526f84ae263888f14a7b47ed52c255c4dacaf
Ben <dia_v3@rambler.ru>
2024-07-27 14:36:06 +0300

    Inclusion tag replaced with custom tag

--- config/endless_menu/templatetags/menu_tags.py
+++ config/endless_menu/templatetags/menu_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 
 from endless_menu.models import MenuElements
 from endless_menu.utils import build_tree
@@ -6,8 +7,22 @@ from endless_menu.utils import build_tree
 register = template.Library()
 
 
-@register.inclusion_tag(filename='navigations/menu.html', takes_context=True)
-def draw_menu(context, menu_name):
-    query_set = MenuElements.objects.select_related('parent').filter(menu__name=menu_name)
-    absolute_url = context.request.build_absolute_uri()
-    return {'tree': build_tree(query_set, absolute_url)}
+class MenuNode(template.Node):
+    def __init__(self, menu_name):
+        self.menu_name = menu_name
+
+    def render(self, context):
+        query_set = MenuElements.objects.select_related('parent').filter(menu__name=self.menu_name)
+        absolute_url = context.request.build_absolute_uri()
+        engine = context.template.engine.get_template(
+            f"{settings.MENU_TEMPLATES}{self.menu_name}.html"
+        )
+        return engine.render(template.Context(
+            {'tree': build_tree(query_set, absolute_url)},
+            autoescape=context.autoescape))
+
+
+@register.tag
+def draw_menu(parser, token):
+    tag_name, menu_name = token.split_contents()
+    return MenuNode(menu_name)
